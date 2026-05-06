3661c33b1280fd4e9b16b28adfe78b43d23fb4de
JodiS34 <147281995+JodiS34@users.noreply.github.com>
2026-05-06 12:43:30 -0400

    Updated code

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import logo from "./logo.svg";
 import React from "react";
 import Weather from "./Weather";
+import Searchengine from "./Searchengine";
+import Footer from "./Footer";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from React</h1>
+        <h1>Weather Search Engine</h1>
         <Weather city="" />
       </header>
     </div>
---
+++ src/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Weather from "./Weather";
+import App from "./App";
+<footer>
+  <small>
+    <a
+      href="github.com/JodiS34/Weather-react-app."
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      Open-source on github
+    </a>
+    , by Jodi Smith , and <a href=""></a>
+  </small>
+</footer>;
